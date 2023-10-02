dfd26b843162df6996d25b15b818516e5e37c2e9
Debiddo <dzheng@terpmail.umd.edu>
2023-10-02 03:22:55 -0400

    Added dynamic link to each menu item

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 # Create your views here.
 
 class MenuList(generic.ListView):
@@ -8,6 +8,13 @@ class MenuList(generic.ListView):
 
     # render the html page using the one line code below
     template_name = "index.html"
+
+    # must name this method exactly like this: this method will display content on the html page
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
+
+        return context
 
 class MenuItemDetail(generic.DetailView):
     model = Item
